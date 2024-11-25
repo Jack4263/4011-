865062a974e601710b99ce4c83ba85a99989ff72
Jack4263 <134334519+Jack4263@users.noreply.github.com>
2024-11-25 16:35:12 +0000

    editing index javascript

--- Home page/index.js
+++ Home page/index.js
@@ -1,27 +1,11 @@
-navjson = "nav.json";
+localJsonFile = "nav.json";
 
-let home = document.querySelector("#home");
+var home = document.querySelector("#home");
 let meet = document.querySelector("#meet");
 let form = document.querySelector("#form");
 let a1 = document.querySelector("#a1");
 let a2 = document.querySelector("#a2");
 let a3 = document.querySelector("#a3");
 let nav = document.querySelector("#navigation");
-
-
-
-
-document.addEventListener('DOMContentLoaded', ()=>{
-    fetch(localJsonFile)
-    .then(response => response.json())
-    .then (responseData =>{
-
-document.addEventListener("DOMContentLoaded", () => {
-  fetch(navjson)
-    .then((response) => response.json())
-    .then((responseData) => {
-      for (item of responseData) {
-        home.textContent = "test";
-      }
-    });
-});
+home.textContent = "Helps improve blood circulation (new list item)";
+console.log(a3);
