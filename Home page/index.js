d0c7d2f120d4c78960052ade553804dfcc4430f9
Jack4263 <jackmcmath10@gmail.com>
2024-11-26 15:11:53 +0000

    merging and changing js

--- Home page/index.js
+++ Home page/index.js
@@ -1,3 +1,9 @@
+navjson = "4011-/nav.json"
+document.addEventListener("DOMContentLoaded", () => {
+    fetch(navjson)
+      .then((response) => response.json())
+      .then((responseData) => {
+    console.log(responseData);})})
 
 document.getElementById("home").textContent = "Home";
 document.getElementById("meet").textContent = "Meet The Team";
