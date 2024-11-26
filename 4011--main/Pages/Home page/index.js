b2b53f29986bd8868b20e5209aa2fd3b6c859586
tosmatosoup <dashtontyler@gmail.com>
2024-11-26 15:04:11 +0000

    Changed file structure + example article json
    changed file structure to be less messy, splitting files into data and pages, ideally as many of the json files should be in Data as possible aswell as image files. The example article data jsons and file structure should make adding new articles very easy as they are all self contained and numbered

---
+++ 4011--main/Pages/Home page/index.js
@@ -0,0 +1,11 @@
+localJsonFile = "nav.json";
+
+var home = document.querySelector("#home");
+let meet = document.querySelector("#meet");
+let form = document.querySelector("#form");
+let a1 = document.querySelector("#a1");
+let a2 = document.querySelector("#a2");
+let a3 = document.querySelector("#a3");
+let nav = document.querySelector("#navigation");
+home.textContent = "Helps improve blood circulation (new list item)";
+console.log(a3);
