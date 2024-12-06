d8877313702bed91652a44b918f31366e4be55f7
Jack4263 <134334519+Jack4263@users.noreply.github.com>
2024-12-06 18:42:36 +0000

    Creating article 2 and 3 - all implemented and resizable

---
+++ Pages/article3/article3.js
@@ -0,0 +1,19 @@
+fetch("Article3.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    document.getElementById("suscities").textContent = data.title;
+    document.getElementById("header1").textContent = data.header1;
+    document.getElementById("header2").textContent = data.header2;
+    document.getElementById("suswhy").textContent = data.header3;
+    document.getElementById("p1").textContent = data.Para1;
+    document.getElementById("p2").textContent = data.Para2;
+    document.getElementById("p3").textContent = data.Para3;
+    document.getElementById("p4").textContent = data.Para4;
+
+    document.getElementById("para1").textContent = data.Para5;
+    document.getElementById("para2").textContent = data.Para6;
+    document.getElementById("para3").textContent = data.Para7;
+    document.getElementById("para4").textContent = data.Para8;
+  });
